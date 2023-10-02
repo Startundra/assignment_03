8e3e23b4c680d406d5f80e8e3521a1275e3a9191
Isaac Reid <ireid@rrc.ca>
2023-10-02 17:56:55 -0500

    Added user selection for main_interface function. Non-functional

--- bank_transactions.py
+++ bank_transactions.py
@@ -8,7 +8,7 @@ Notes: ATM width is 40 chrs
 import random as r
 transaction_optiosn = ["D", "W", "Q",]
 
-users_current_balance = float(r.randint(-1000, 10000))
+users_current_balance = float(r.randint(-100000, 1000000))/10000
 
 user_is_quitter = False
 
@@ -16,17 +16,50 @@ user_is_quitter = False
 # gets users choice from the manu
 def interface_main():
     print(
-        """
+        f"""
         ***************************************         
                 PIXELL RIVER FINANCIAL
                     ATM Simulator
-        Your current balance is: $#,###.##
+        Your current balance is: ${users_current_balance:5}
 
                     Deposit: D
                     Withdraw: W
                     To Quit: Q
         ***************************************
         """)
+    done = False
+    while not done: 
+        try: 
+            menu_choice = input("Enter your selection: ").upper()
+        except:
+            print("""
+                ***************************************
+                            INVALID SELECTION
+                ***************************************
+                  """)
+        else:
+            if menu_choice[0] == "D":
+                return "D"
+            elif menu_choice[0] == "W":
+                return "W"
+            elif menu_choice[0] == "Q":
+                #return "Q"
+                quit()
+            else:
+                print(
+                    """
+                    ***************************************
+                                INVALID SELECTION
+                    ***************************************
+                    """
+                )
+            
+                
+
 
 while not user_is_quitter:
-    break
+    interface_main()
+
+
+
+quit()
