9638b3c2aa8bde975b4446e79260b7135bc44fde
Isaac Reid <ireid@rrc.ca>
2023-10-02 17:11:32 -0500

    initial commit, created 5 files, interest_update, bank_transaction, account_balance, .gitignore, README

---
+++ bank_transactions.py
@@ -0,0 +1,6 @@
+"""
+Description: <<placeholder>>
+Author: Isaac Reid
+Date: 27/09/23
+Usage: Spread on bread like butter (or run in the terminal)
+"""
---
+++ interest_update.py
@@ -0,0 +1,6 @@
+"""
+Description: <<placeholder>>
+Author: Isaac Reid 
+Date: 01/10/23
+Usage: Run in the terminal
+"""
