15e1e9bb9317302f85dda237630c9740d093791a
Isaac Reid <ireid@rrc.ca>
2023-10-02 20:14:14 -0500

    added account reading to interest_update.py

--- interest_update.py
+++ interest_update.py
@@ -4,3 +4,33 @@ Author: Isaac Reid
 Date: 01/10/23
 Usage: Run in the terminal
 """
+
+account_balances = {}
+
+
+
+def read_account_balances():
+
+    account_balances_file = open("account_balances.txt")
+
+    for line in account_balances_file:
+        global account_balances
+        account = ""
+        balance = ""
+        flag_balance = False
+
+        #
+        for char in line:
+
+            if char == "|": 
+                flag_balance = True
+            
+            elif flag_balance:
+                balance += str(char)
+
+            else:
+                account += str(char)
+
+        account_balances.update({account:balance})
+
+read_account_balances()
